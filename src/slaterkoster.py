171322d52e94223eebba63ce201330ed359d8a02
Marius Jonsson <king.oslo@gmail.com>
2022-03-18 21:46:56 +0100

    misc

---
+++ src/slaterkoster.py
@@ -0,0 +1,19 @@
+class Slaterkoster:
+
+    def __init__(self):
+        pass
+
+
+    # calculates
+    def coeff(self,qn1,d,qn2):
+        (l1,m1) = qn1
+        (l2,m2) = qn2
+        # provided a 
+
+
+
+slaterkoster = Slaterkoster()
+
+
+
+coeff = slaterkoster.coeff(d)
