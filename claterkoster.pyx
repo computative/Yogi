c6890d00d9f8220c6cb5fd489360792e1ca43f4c
Marius Jonsson <marius@passopp.passopp>
2023-12-24 14:21:01 +0100

    converted to ase crystal

--- claterkoster.pyx
+++ claterkoster.pyx
@@ -5,17 +5,17 @@ import utils
 from libc.math cimport exp, pow
 cnp.import_array()
 
-def HGamma_m( int k, cnp.ndarray[cnp.float64_t, ndim=1] G, int N, double[:] param, cnp.ndarray[cnp.float64_t, ndim=2] R ):
-    cdef double _ro = 2.35
-    cdef double _rc = 3.8661
-    cdef double _n = 1.9771
-    cdef double _nc = 6.8702
+def HGamma( int k, cnp.ndarray[cnp.float64_t, ndim=1] G, int N, double[:] param, double[:] gsp_param, cnp.ndarray[cnp.float64_t, ndim=2] R ):
+    cdef double _ro = gsp_param[0]
+    cdef double _rc = gsp_param[1]
+    cdef double _n = gsp_param[2]
+    cdef double _nc = gsp_param[3]
     cdef double g = np.linalg.norm(G)
     cdef cnp.ndarray[cnp.float64_t, ndim=1] r = np.zeros(3, dtype=np.float64)
     cdef cnp.ndarray[cnp.complex128_t, ndim=2] HGamma = np.zeros( (N,N) , dtype=np.complex128 )
     # forst loop over offsite 
     cdef int i, j, L
-    cdef double l, m, n, bowler
+    cdef double l, m, n, gsp
     cdef double IrI, x, y
     x = exp( -_n*pow(_rc,-_nc) )
     y = pow(_ro,_n)*exp(_n* pow( _ro/_rc, _nc) )
@@ -28,23 +28,23 @@ def HGamma_m( int k, cnp.ndarray[cnp.float64_t, ndim=1] G, int N, double[:] para
             IrI = np.linalg.norm(r)
             e = r/IrI
             l,m,n = e[0], e[1], e[2]
-            bowler = y*pow(x,(pow(IrI,_nc)))*pow(IrI,-_n)
-            # what is bowler when r=g=0?
+            gsp = y*pow(x,(pow(IrI,_nc)))*pow(IrI,-_n)
+            # what is gsp when r=g=0?
             HGamma[4*i:4*(i+1), 4*j:4*(j+1)] = np.array([
-                [ bowler*param[2],      bowler*param[3]*l,                              bowler*param[3]*m,                              bowler*param[3]*n],
-                [-bowler*param[3]*l,    bowler*param[4]*l*l + bowler*param[5]*(1-l*l ), bowler*m*l*(param[4]-param[5]),                 bowler*n*l*(param[4] - param[5])],
-                [-bowler*param[3]*m,    bowler*l*m*(param[4] - param[5]),               bowler*param[4]*m*m + bowler*param[5]*(1-m*m),  bowler*n*m*(param[4] - param[5])],
-                [-bowler*param[3]*n,    bowler*l*n*(param[4] - param[5]),               bowler*m*n*(param[4]-param[5]),                 bowler*param[4]*n*n + bowler*param[5]*(1-n*n)]])
+                [ gsp*param[2],      gsp*param[3]*l,                              gsp*param[3]*m,                              gsp*param[3]*n],
+                [-gsp*param[3]*l,    gsp*param[4]*l*l + gsp*param[5]*(1-l*l ), gsp*m*l*(param[4]-param[5]),                 gsp*n*l*(param[4] - param[5])],
+                [-gsp*param[3]*m,    gsp*l*m*(param[4] - param[5]),               gsp*param[4]*m*m + gsp*param[5]*(1-m*m),  gsp*n*m*(param[4] - param[5])],
+                [-gsp*param[3]*n,    gsp*l*n*(param[4] - param[5]),               gsp*m*n*(param[4]-param[5]),                 gsp*param[4]*n*n + gsp*param[5]*(1-n*n)]])
     if g > utils.eps:
         for i in range(L):
             l, m, n = G/g
-            # what is bowler when r=g=0?
-            bowler = y*pow(x,(pow(g,_nc)))*pow(g,-_n)
+            # what is gsp when r=g=0?
+            gsp = y*pow(x,(pow(g,_nc)))*pow(g,-_n)
             HGamma[4*i:4*(i+1),4*i:4*(i+1)] = np.array([
-                [ bowler*param[2],      bowler*param[3]*l,                              bowler*param[3]*m,                              bowler*param[3]*n],
-                [-bowler*param[3]*l,    bowler*param[4]*l*l + bowler*param[5]*(1-l*l),  bowler*m*l*(param[4]-param[5]),                 bowler*n*l*(param[4] - param[5])],
-                [-bowler*param[3]*m,    bowler*l*m*(param[4] - param[5]),               bowler*param[4]*m*m + bowler*param[5]*(1-m*m),  bowler*n*m*(param[4] - param[5])],
-                [-bowler*param[3]*n,    bowler*l*n*(param[4] - param[5]),               bowler*m*n*(param[4]-param[5]),                 bowler*param[4]*n*n + bowler*param[5]*(1-n*n)]])
+                [ gsp*param[2],      gsp*param[3]*l,                              gsp*param[3]*m,                              gsp*param[3]*n],
+                [-gsp*param[3]*l,    gsp*param[4]*l*l + gsp*param[5]*(1-l*l),  gsp*m*l*(param[4]-param[5]),                 gsp*n*l*(param[4] - param[5])],
+                [-gsp*param[3]*m,    gsp*l*m*(param[4] - param[5]),               gsp*param[4]*m*m + gsp*param[5]*(1-m*m),  gsp*n*m*(param[4] - param[5])],
+                [-gsp*param[3]*n,    gsp*l*n*(param[4] - param[5]),               gsp*m*n*(param[4]-param[5]),                 gsp*param[4]*n*n + gsp*param[5]*(1-n*n)]])
     else:
         for i in range(L):
             HGamma[4*i:4*(i+1),4*i:4*(i+1)] = np.diag([param[0], param[1], param[1], param[1]]) 
--- hamiltonian.py
+++ hamiltonian.py
@@ -1,78 +1,68 @@
-from claterkoster import HGamma_m
+from claterkoster import HGamma
 import numpy as np
-from numpy import array as ar
 from multiprocessing import Pool
 import utils
 import scipy as sci
+import ase
+from ase.lattice.cubic import Diamond
 
 class Hamiltonian:
 
-    def __init__(self, crystal, functions, param):
-        # init
+    def __init__(self, atoms, functions, param, gsp):
 
-        param = np.array([param["Es"],param["Ep"],param["ss-sigma"],\
-                param["sp-sigma"],param["pp-sigma"],param["pp-pi"]])
-        self.crystal = crystal
-        # find W by transforming cubic coords
-        a1,a2,a3 = self.crystal.lattice
+        self.N = len(atoms)*len(functions)
+        a1,a2,a3 = atoms.cell[:]
         A = np.array([a1,a2,a3]).T
         
-        neighbors = 1
-        self.neighbors=[]
-        for i in range(-neighbors, neighbors+1):
-            for j in range(-neighbors, neighbors+1):
-                for k in range(-neighbors, neighbors+2):
-                    self.neighbors.append(  np.dot(A, ar([ i, j, k]) ) )
-        self.N = len(self.crystal)*len(functions)
-        R = ar(list(list(self.crystal.nuclei.values())[0].values()))
+        num_neighbors = 1
+        self.neighbors = []
+        for i in range(-num_neighbors, num_neighbors+1):
+            for j in range(-num_neighbors, num_neighbors+1):
+                for k in range(-num_neighbors, num_neighbors+1):
+                    self.neighbors.append(  np.dot(A, np.array([i, j, k]) ) )
+        R = atoms.get_positions()
+        
         with Pool() as pool:
-            self.HGamma = ar( pool.starmap( HGamma_m, [ (  k, G,self.N, param, R)  for k, G in enumerate(self.neighbors)] ) )
+            param = np.array([param[key] for key in 
+                              ["Es","Ep","ss-sigma","sp-sigma","pp-sigma","pp-pi"]])
+            gsp_param = np.array([gsp[key] for key in ["r0","rc","n","nc"]])
+            self.HGamma = np.array(pool.starmap( HGamma, [
+                (k, G,self.N, param, gsp_param, R) for k, G in enumerate(self.neighbors)] ) )
 
     def __call__(self, k):
-        N = self.N
-        H = np.zeros( (N,N) , dtype=complex )
+        H = np.zeros( (self.N,self.N) , dtype=complex )
         for i, G in enumerate(self.neighbors):
             H += np.exp(1j*np.dot( G, k ))*self.HGamma[i]
         return H
 
 if __name__ == "__main__":
-    from crystal import Crystal
 
-    a = 5.431
-    rep = [[a,0,0], [0,a,0], [0,0,a]]
-    atoms = {"Si": [ 
-        [0,0,0],
-        [a/4,a/4,a/4],
-        [0,a/2,a/2],
-        [a/4,3*a/4,3*a/4],
-        [a/2,0,a/2],
-        [3*a/4,a/4,3*a/4],
-        [a/2,a/2,0],
-        [3*a/4,3*a/4,a/4]
-    ]}
-
-    coords = {"rep" : rep, "atoms" : atoms}
-
-    crl = Crystal(dims = (1,1,1)).from_coords(coords)
+    atoms = Diamond(
+        directions=[[1,0,0], [0,1,0], [0,0,1]],
+        symbol='Si',
+        size=(1, 1, 1),
+        latticeconstant=5.431,
+        pbc=True
+    )
 
     bowler = {  "Es": -12.2, "Ep": -5.75, 
                 "ss-sigma": -1.938, "sp-sigma": 1.745,
                 "pp-sigma": 3.050, "pp-pi": -1.075 }
-
-
-    hamiltonian = Hamiltonian(crl, ["1s","2px","2py","2pz"], bowler)
-
-    eigs = []
+    gsp = {"r0": 2.35, "rc": 3.8661,"n": 1.9771,"nc": 6.8702 }
+    basis = ["1s","2px","2py","2pz"]
+    hamiltonian = Hamiltonian(atoms, basis, bowler, gsp)
 
     from visualtools import VisualTools
     
-    a1,a2,a3 = rep[0], rep[1], rep[2]
+    a1,a2,a3 = atoms.cell[:]
+    
     sympts = VisualTools.fcc_sympts(*VisualTools.reciprocal(a1,a2,a3))
     kpath = {
         "waypts": [ sympts["K"], sympts["Gamma"],sympts["L"], sympts["K"]  ],
         "n" : [35,35,35]
     }
 
+    eigs = []
     for i, k in enumerate(VisualTools.kpts(kpath["waypts"], kpath["n"])):
         print( "Iteration", i )
         HMatrix = hamiltonian( k )
@@ -80,8 +70,8 @@ if __name__ == "__main__":
         if np.linalg.norm(k) == 0:
             VisualTools.matrix_formatting()
             print( np.round( HMatrix,3 ))
-        if not sci.linalg.ishermitian(HMatrix):
-            raise(ValueError("Hamiltonian nonhermitian"))
+        if not sci.linalg.ishermitian(HMatrix, atol=1e-10): 
+            raise(RuntimeError("Hamiltonian nonhermitian"))
         eigs.append(np.linalg.eigvalsh( HMatrix, "L"))
 
     bands = np.array(eigs).T
